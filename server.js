c8e1b7c14c0501d2c0007dda17bb4aa712cac618
AhmedNasser1324 <mfgtwo1313@gmail.com>
2023-02-08 04:49:40 +0200

    initial commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const server = require("http").createServer(app);
 const PORT = 5001;
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://rapperd-chat.onrender.com",
+    origin: "https://backchatapp.onrender.com",
     methods: ["GET", "POST"],
   },
 });
